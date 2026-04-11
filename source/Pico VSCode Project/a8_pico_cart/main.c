28f6953dfb9e1002f939e5d2d1904ca3e3ffd222
marushio-rima <50846225+marushio-rima@users.noreply.github.com>
2026-04-11 02:18:53 -0300

    Update main.c
    Port: update main.c header and PHI2 comment for WeAct RP2350B (GP37)

--- source/Pico VSCode Project/a8_pico_cart/main.c
+++ source/Pico VSCode Project/a8_pico_cart/main.c
@@ -1,17 +1,18 @@
 /**
- *    _   ___ ___ _       ___          _   
- *   /_\ ( _ ) _ (_)__ _ / __|__ _ _ _| |_ 
- *  / _ \/ _ \  _/ / _/_\ (__/ _` | '_|  _|
- * /_/ \_\___/_| |_\__\_/\___\__,_|_|  \__|
- *                                         
- * 
- * Atari 8-bit cartridge for Raspberry Pi Pico (16MB clone with all GPIO)
+ *    _   ___ ___ _       ___    ___          _   
+ *   /_\ ( _ ) _ (_)__ _ |_  )/ __|__ _ _ _| |_ 
+ *  / _ \/ _ \  _/ / _/_\/ / | (__/ _` | '_|  _|
+ * /_/ \_\___/_| |_\__\_/___| \___\__,_|_|  \__|
  *
- * Robin Edwards 2023
+ * Atari 8-bit cartridge for WeAct RP2350B
+ * Based on A8PicoCart by Robin Edwards 2023
+ * Port by marushio-rima 2025
  *
  * Needs to be a release NOT debug build for the cartridge emulation to work
- * 
- * Edit myboard.h depending on the type of flash memory on the pico clone
+ *
+ * Boot detection: checks PHI2 (GP37) for 100ms
+ * If PHI2 is high → plugged into Atari → cartridge emulation mode
+ * If PHI2 is low  → powered from USB → USB Mass Storage mode
  */
 
 #include <stdlib.h>
@@ -29,115 +30,79 @@ void cdc_task(void);
 
 int main(void)
 {
-    // check to see if we are plugged into Atari 8-bit
-    // by checking for high on PHI2 gpio for 100ms
+    // Check if plugged into Atari 8-bit
+    // by checking for high on PHI2 (GP37 on WeAct RP2350B) for 100ms
     gpio_init(ATARI_PHI2_PIN);
     gpio_set_dir(ATARI_PHI2_PIN, GPIO_IN);
     while (to_ms_since_boot(get_absolute_time()) < 100)
     {
-      if (gpio_get(ATARI_PHI2_PIN))
-        atari_cart_main();
+        if (gpio_get(ATARI_PHI2_PIN))
+            atari_cart_main();
     }
 
-  // we are presumably powered from USB
-  // enter USB mass storage mode
+    // Powered from USB — enter USB Mass Storage mode
+    stdio_init_all();
+    printf("A8Pico2Cart - USB Mass Storage mode\n");
 
-  stdio_init_all();   // for serial output, via printf()
-  printf("Start up\n");  
+    // Init device stack on configured roothub port
+    tud_init(BOARD_TUD_RHPORT);
 
-  // init device stack on configured roothub port
-  tud_init(BOARD_TUD_RHPORT);
+    while (1)
+    {
+        tud_task();
+        cdc_task();
+    }
 
-  while (1)
-  {
-    tud_task(); // tinyusb device task
-
-    cdc_task();
-  }
-
-  return 0;
+    return 0;
 }
 
 //--------------------------------------------------------------------+
 // Device callbacks
 //--------------------------------------------------------------------+
 
-// Invoked when device is mounted
 void tud_mount_cb(void)
 {
-  printf("Device mounted\n"); 
-  if (!mount_fatfs_disk())
-    create_fatfs_disk();
+    printf("Device mounted\n");
+    if (!mount_fatfs_disk())
+        create_fatfs_disk();
 }
 
-// Invoked when device is unmounted
 void tud_umount_cb(void)
 {
-  printf("Device unmounted\n");  
+    printf("Device unmounted\n");
 }
 
-// Invoked when usb bus is suspended
-// remote_wakeup_en : if host allow us  to perform remote wakeup
-// Within 7ms, device must draw an average of current less than 2.5 mA from bus
 void tud_suspend_cb(bool remote_wakeup_en)
 {
-  (void) remote_wakeup_en;
-//  blink_interval_ms = BLINK_SUSPENDED;
+    (void) remote_wakeup_en;
 }
 
-// Invoked when usb bus is resumed
 void tud_resume_cb(void)
 {
-//  blink_interval_ms = BLINK_MOUNTED;
 }
-
 
 //--------------------------------------------------------------------+
 // USB CDC
 //--------------------------------------------------------------------+
 void cdc_task(void)
 {
-  // connected() check for DTR bit
-  // Most but not all terminal client set this when making connection
-  // if ( tud_cdc_connected() )
-  {
-    // connected and there are data available
-    if ( tud_cdc_available() )
+    if (tud_cdc_available())
     {
-      // read data
-      char buf[64];
-      uint32_t count = tud_cdc_read(buf, sizeof(buf));
-      (void) count;
-
-      // Echo back
-      // Note: Skip echo by commenting out write() and write_flush()
-      // for throughput test e.g
-      //    $ dd if=/dev/zero of=/dev/ttyACM0 count=10000
-      tud_cdc_write(buf, count);
-      tud_cdc_write_flush();
+        char buf[64];
+        uint32_t count = tud_cdc_read(buf, sizeof(buf));
+        (void) count;
+        tud_cdc_write(buf, count);
+        tud_cdc_write_flush();
     }
-  }
 }
 
-// Invoked when cdc when line state changed e.g connected/disconnected
 void tud_cdc_line_state_cb(uint8_t itf, bool dtr, bool rts)
 {
-  (void) itf;
-  (void) rts;
-
-  // TODO set some indicator
-  if ( dtr )
-  {
-    // Terminal connected
-  }else
-  {
-    // Terminal disconnected
-  }
+    (void) itf;
+    (void) rts;
 }
 
-// Invoked when CDC interface received data from host
 void tud_cdc_rx_cb(uint8_t itf)
 {
-  (void) itf;
+    (void) itf;
 }
-
