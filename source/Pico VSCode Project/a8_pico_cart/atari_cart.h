ee80b6b0e80178ca94e683c5af72ab26e34e54fa
marushio-rima <50846225+marushio-rima@users.noreply.github.com>
2026-04-11 02:01:28 -0300

    Update atari_cart.h

--- source/Pico VSCode Project/a8_pico_cart/atari_cart.h
+++ source/Pico VSCode Project/a8_pico_cart/atari_cart.h
@@ -1,21 +1,19 @@
 /**
- *    _   ___ ___ _       ___          _   
- *   /_\ ( _ ) _ (_)__ _ / __|__ _ _ _| |_ 
- *  / _ \/ _ \  _/ / _/_\ (__/ _` | '_|  _|
- * /_/ \_\___/_| |_\__\_/\___\__,_|_|  \__|
- *                                         
- * 
- * Atari 8-bit cartridge for Raspberry Pi Pico
+ *    _   ___ ___ _       ___    ___          _   
+ *   /_\ ( _ ) _ (_)__ _ |_  )/ __|__ _ _ _| |_ 
+ *  / _ \/ _ \  _/ / _/_\/ / | (__/ _` | '_|  _|
+ * /_/ \_\___/_| |_\__\_/___| \___\__,_|_|  \__|
  *
- * Robin Edwards 2023
- *
- * Needs to be a release NOT debug build for the cartridge emulation to work
+ * Atari 8-bit cartridge for WeAct RP2350B
+ * Based on A8PicoCart by Robin Edwards 2023
+ * Port by marushio-rima 2025
  */
 
 #ifndef __ATARI_CART_H__
 #define __ATARI_CART_H__
 
-#define ATARI_PHI2_PIN        22    // used on boot to check if we are plugged into an atari or usb
+// WeAct RP2350B: PHI2 remapped from GP22 to GP37
+#define ATARI_PHI2_PIN   37
 
 void atari_cart_main();
 
